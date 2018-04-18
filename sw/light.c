62997f7b62644c91e27163c29aed75652c05e294
Matthias Larisch <mail@matthias-larisch.de>
2018-04-18 11:22:39 +0200

    Add more staircase lights

--- sw/light.c
+++ sw/light.c
@@ -14,9 +14,16 @@ void light_init(void)
   if(config_get_uint(CONFIG_HAS_LIGHT_CONTROL))
   {
     pwm_init();
+    /* Hallway light beginning from door .. backyard */
     for(int i = 1; i <= 5; ++i)
     {
       pwm_set_dc(i, 150);
     }
+    /* K20-1 staircase light */
+    pwm_set_dc(6, 1500);
+    /* K20-2 staircase light */
+    pwm_set_dc(7, 40);
+
+    /* GPIOB 3 is motion sensor input, active high */
   }
 }
