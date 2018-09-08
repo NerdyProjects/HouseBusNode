ab6718300c1721e2e8c2bdab43190e6119f2bb95
Matthias Larisch <mail@matthias-larisch.de>
2018-09-08 16:27:45 +0200

    Fix key handling, test burner/circulation

--- sw/nodes/viessmann_trimatik.c
+++ sw/nodes/viessmann_trimatik.c
@@ -41,15 +41,49 @@ typedef struct {
   float c2;
 } correction_coefficient_t;
 
+enum
+{
+  KEY_MODE = 1
+};
+
+enum
+{
+  KEY_MODE_DEBUG = 6
+};
+
 static correction_coefficient_t pt500[3];
 static int16_t temperature[3];
+/* keys:
+ * 0 2 4
+ * 1 3 5
+ *
+ * each from 0-15, where labels are as follows:
+ * 0 (Sun): -7 .. +8 (0 is N)
+ * 2 (Moon): -9 .. +6 (0 is N)
+ * 4 (Water): 0, 32 .. 50 (each step is +2)
+ * 1 (Mode): 0 Freeze Freeze, 1 Sun Freeze, 2 Sun Moon, 3 Sun Sun, 4 Moon Moon, 5 Water Freeze, 6..15: 1..10
+ * 3 (slope): 0,2 .. 2,6 (each step is +0,2)
+ * 5 (offset): -12 .. +33 (each step is +3)
+ */
 static uint8_t key[6];
 static uint8_t key_next[6];
 
 
-/* slowest filter needs ~30 ms to settle, that is 6 5ms ticks. add one each for synchronisation and AD conversion */
-#define ANALOG_FILTER_TICKS 8
+/* slowest filter needs ~30 ms to settle, that is 6 5ms ticks.
+ * add one each for synchronisation and AD conversion
+ * and 5 for digital filter reset/settling */
+#define ANALOG_FILTER_TICKS 13
 volatile uint8_t analog_evaluate_in_ticks;
+
+static void burner(uint8_t enable)
+{
+  palWritePad(GPIOA, GPIOA_K1_FIRE, enable ? PAL_LOW : PAL_HIGH);
+}
+
+static void circulation(uint8_t enable)
+{
+  palWritePad(GPIOA, GPIOA_K2_20A, enable ? PAL_LOW : PAL_HIGH);
+}
 
 static void analog_pullup(uint8_t enable)
 {
@@ -60,6 +94,28 @@ static void analog_pullup(uint8_t enable)
   else
   {
     palSetPad(GPIOA, GPIOA_PU_OFF);
+  }
+}
+
+static void led_a(uint8_t enable)
+{
+  if(enable)
+  {
+    palSetPad(GPIOA, GPIOA_LED_A);
+  } else
+  {
+    palClearPad(GPIOA, GPIOA_LED_A);
+  }
+}
+
+static void led_b(uint8_t enable)
+{
+  if(enable)
+  {
+    palSetPad(GPIOB, GPIOB_LED_GREEN);
+  } else
+  {
+    palClearPad(GPIOB, GPIOB_LED_GREEN);
   }
 }
 
@@ -77,6 +133,12 @@ static int16_t pt500_adc_to_centicelsius(uint16_t raw_adc, correction_coefficien
    * -2.6535434581358079e+000
    * 6.4705225905651158e-005
    * -> should be calibrated in application per channel
+   * Calibration using room temperature and old pipe thermometer:
+   * 18-19 degrees in basement and 21-22 outside:
+   * 33910 33770 33935
+   * 64 degrees burner/pipe:
+   * 33910 35620 36420
+   * (results seem really bad :( )
    */
   float u = qfp_int2float(raw_adc);
   float t = qfp_fadd(coef->c0, qfp_fadd(qfp_fmul(coef->c1, u), qfp_fmul(qfp_fmul(u, u), coef->c2)));
@@ -94,14 +156,15 @@ void app_init(void)
   palSetPadMode(GPIOB, GPIOB_SW_C34, PAL_MODE_OUTPUT_PUSHPULL);
   palSetPadMode(GPIOB, GPIOB_SW_C56, PAL_MODE_OUTPUT_PUSHPULL);
   palClearPort(GPIOB, PAL_PORT_BIT(GPIOB_SW_C12) | PAL_PORT_BIT(GPIOB_SW_C34) | PAL_PORT_BIT(GPIOB_SW_C56));
-  palSetPadMode(GPIOA, GPIOA_SW_B1_135, PAL_MODE_INPUT_PULLUP);
-  palSetPadMode(GPIOA, GPIOA_SW_B2_135, PAL_MODE_INPUT_PULLUP);
-  palSetPadMode(GPIOB, GPIOB_SW_B0_135, PAL_MODE_INPUT_PULLUP);
-  palSetPadMode(GPIOB, GPIOB_SW_B3_135, PAL_MODE_INPUT_PULLUP);
-  palSetPadMode(GPIOC, GPIOC_SW_B0_246, PAL_MODE_INPUT_PULLUP);
-  palSetPadMode(GPIOC, GPIOC_SW_B1_246, PAL_MODE_INPUT_PULLUP);
-  palSetPadMode(GPIOF, GPIOF_SW_B2_246, PAL_MODE_INPUT_PULLUP);
-  palSetPadMode(GPIOF, GPIOF_SW_B3_246, PAL_MODE_INPUT_PULLUP);
+  palSetPadMode(GPIOA, GPIOA_SW_B1_135, PAL_MODE_INPUT_PULLDOWN);
+  palSetPadMode(GPIOA, GPIOA_SW_B2_135, PAL_MODE_INPUT_PULLDOWN);
+  palSetPadMode(GPIOB, GPIOB_SW_B0_135, PAL_MODE_INPUT_PULLDOWN);
+  palSetPadMode(GPIOB, GPIOB_SW_B3_135, PAL_MODE_INPUT_PULLDOWN);
+  palSetPadMode(GPIOC, GPIOC_SW_B0_246, PAL_MODE_INPUT_PULLDOWN);
+  palSetPadMode(GPIOC, GPIOC_SW_B1_246, PAL_MODE_INPUT_PULLDOWN);
+  palSetPadMode(GPIOF, GPIOF_SW_B2_246, PAL_MODE_INPUT_PULLDOWN);
+  palSetPadMode(GPIOF, GPIOF_SW_B3_246, PAL_MODE_INPUT_PULLDOWN);
+  palSetPad(GPIOB, GPIOB_SW_C12);
 }
 
 void debug_keys(void)
@@ -129,29 +192,37 @@ void app_fast_tick(void)
         (palReadPad(GPIOA, GPIOA_SW_B2_135) << 2) |
         (palReadPad(GPIOB, GPIOB_SW_B3_135) << 3);
     uint8_t inB =
-        palReadPad(GPIOC, GPIOC_SW_B0_246) |
-        (palReadPad(GPIOC, GPIOC_SW_B1_246) << 1) |
-        (palReadPad(GPIOF, GPIOF_SW_B2_246) << 2) |
-        (palReadPad(GPIOF, GPIOF_SW_B3_246) << 3);
+        (palReadPad(GPIOC, GPIOC_SW_B0_246) << 3) |
+        (palReadPad(GPIOC, GPIOC_SW_B1_246) << 2) |
+        (palReadPad(GPIOF, GPIOF_SW_B2_246) << 1) |
+        (palReadPad(GPIOF, GPIOF_SW_B3_246));
+    if(key_next[currentKey*2] == inA)
+    {
+      key[currentKey*2] = inA;
+    } else
+    {
+      key_next[currentKey*2] = inA;
+    }
+    if(key_next[currentKey*2+1] == inB)
+    {
+      key[currentKey*2+1] = inB;
+    } else {
+      key_next[currentKey*2+1] = inB;
+    }
+
     switch(currentKey)
     {
     case 0:
-      key[0] = inA;
-      key[1] = inB;
       currentKey = 1;
       palClearPad(GPIOB, GPIOB_SW_C12);
       palSetPad(GPIOB, GPIOB_SW_C34);
       break;
     case 1:
-      key[2] = inA;
-      key[3] = inB;
       currentKey = 2;
       palClearPad(GPIOB, GPIOB_SW_C34);
       palSetPad(GPIOB, GPIOB_SW_C56);
       break;
     case 2:
-      key[4] = inA;
-      key[5] = inB;
       currentKey = 0;
       palClearPad(GPIOB, GPIOB_SW_C56);
       palSetPad(GPIOB, GPIOB_SW_C12);
@@ -164,12 +235,19 @@ void app_fast_tick(void)
   if(analog_evaluate_in_ticks)
   {
     analog_evaluate_in_ticks--;
+    if(analog_evaluate_in_ticks == 5)
+    {
+      analog_filter_reset((1 << 1) | (1 << 2) | (1 << 3));
+    }
     if(analog_evaluate_in_ticks == 0)
     {
       temperature[0] = pt500_adc_to_centicelsius(adc_smp_filtered[1], &pt500[0]);
       temperature[1] = pt500_adc_to_centicelsius(adc_smp_filtered[2], &pt500[1]);
       temperature[2] = pt500_adc_to_centicelsius(adc_smp_filtered[3], &pt500[2]);
-      debug_analog();
+      if(key[KEY_MODE] == KEY_MODE_DEBUG)
+      {
+        debug_analog();
+      }
       analog_pullup(0);
     }
   }
@@ -177,9 +255,14 @@ void app_fast_tick(void)
 
 void app_tick(void)
 {
+  static uint8_t led_count = 0;
   analog_pullup(1);
   analog_evaluate_in_ticks = ANALOG_FILTER_TICKS;
   debug_keys();
-
+  led_a(led_count & 1);
+  led_b(led_count & 2);
+  led_count++;
+  burner(key[1] == 6);
+  circulation(key[3] == 1);
 }
 #endif
