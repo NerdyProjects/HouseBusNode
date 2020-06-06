56d4e4125708e639d682b215745737ca154b073e
Tilmann Becker <tilmann.becker@gmail.com>
2020-06-06 11:08:33 +0200

    remove source file reference from generated dsdl files

--- sw/compile_dsdl.sh
+++ sw/compile_dsdl.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
 ./libcanard/dsdl_compiler/libcanard_dsdlc --outdir dsdl ../dsdl/homeautomation
+
+# Remove source file reference from generated files because they contain absolute paths
+find dsdl -type f -name '*.[ch]' -exec sed -i '/Source file:/d' {} \;
+
